831cf40e292cb3f88697c9f42d804107bfe33d4b
Jim Madge <jmmadge@gmail.com>
2020-11-20 11:18:54 +0000

    Add unhealthy container test

--- tests/test_healthy.py
+++ tests/test_healthy.py
@@ -21,21 +21,19 @@ def healthy_container(tmp_path, docker_client):
     dockerfile.write_text(dedent("""\
         FROM alpine:latest
 
-        RUN apk add --no-cache curl
-
-        HEALTHCHECK --interval=10s CMD curl -f https://archlinux.org
+        HEALTHCHECK --interval=10s CMD true
     """))
 
     # Create 'healthy' container
     docker_client.images.build(
         path=str(d),
         rm=True,
-        tag="healthy"
+        tag="healthy_image"
     )
     container = docker_client.containers.run(
-        "healthy",
+        "healthy_image",
         command="sleep 2m",
-        name="healthy",
+        name="healthy_container",
         detach=True,
         auto_remove=True
     )
@@ -50,11 +48,61 @@ def healthy_container(tmp_path, docker_client):
 
     # Stop container and remove image
     container.stop()
-    docker_client.images.remove("healthy")
+    docker_client.images.remove("healthy_image")
+
+
+@pytest.fixture
+def unhealthy_container(tmp_path, docker_client):
+    # Create temporary directory
+    d = tmp_path / "unhealthy"
+    d.mkdir()
+
+    # Write 'unhealthy' Dockerfile
+    dockerfile = d / "Dockerfile"
+    dockerfile.write_text(dedent("""\
+        FROM alpine:latest
+
+        HEALTHCHECK --interval=10s CMD false
+    """))
+
+    # Create 'unhealthy' container
+    docker_client.images.build(
+        path=str(d),
+        rm=True,
+        tag="unhealthy_image"
+    )
+    container = docker_client.containers.run(
+        "unhealthy_image",
+        command="sleep 2m",
+        name="unhealthy_container",
+        detach=True,
+        auto_remove=True
+    )
+
+    # Wait for container to start
+    container.reload()
+    while container.attrs["State"]["Health"]["Status"] == "starting":
+        sleep(1)
+        container.reload()
+
+    yield container
+
+    # Stop container and remove image
+    container.stop()
+    docker_client.images.remove("unhealthy_image")
 
 
 def test_healthy(capsys, healthy_container):
     health_check(healthy_container)
 
     captured = capsys.readouterr()
-    assert captured.out.strip() == "healthy - healthy - skipping"
+    assert captured.out.strip() == "healthy_container - healthy - skipping"
+
+
+def test_unhealthy(capsys, unhealthy_container):
+    health_check(unhealthy_container)
+
+    captured = capsys.readouterr()
+    assert (
+        captured.out.strip() == "unhealthy_container - unhealthy - restarting"
+    )
